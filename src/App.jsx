0e28bdf7981dceed155e2d5ebbeef01eba47b22c
aboublef <kristopherab@gmail.com>
2022-11-01 11:04:53 +0000

    using both inline and apply

--- src/App.jsx
+++ src/App.jsx
@@ -13,10 +13,11 @@ export default function App() {
 }
 
 function Card() {
+    // hover:bg-neutral-600
     return (
-        <section>
-            <img src="http://placekitten.com/100/100" alt="cat" />
-            <p>
+        <section class="rounded-md border border-neutral-500 bg-neutral-700 hover:bg-neutral-600 transition-colors text-neutral-100 flex flex-row items-start cursor-pointer">
+            <img class="card-img" src="http://placekitten.com/100/100" alt="cat" />
+            <p class="card-contents">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
                 tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
                 quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
@@ -25,3 +26,13 @@ function Card() {
         </section>
     );
 }
+
+/**!SECTION
+ * "rounded-md border border-neutral-500 bg-neutral-700 hover:bg-neutral-600 transition-colors text-neutral-100 flex flex-row items-start cursor-pointer">
+
+  <img src="http://placekitten.com/100/100" alt="cat" class="py-4 pl-4 aspect-square" />
+
+  <p class="mx-4 my-3 leading-4">
+
+    Lorem ipsum dolor sit
+ */
